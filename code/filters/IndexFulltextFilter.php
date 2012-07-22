f2b100aa6af49b18a11b04c6c750ba1057568d71
Andrew Short <andrewjshort@gmail.com>
2012-07-23 02:15:58 +1000

    NEW: Add a search filter for matching against an index.

---
+++ code/filters/IndexFulltextFilter.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * A search filter which operates against an index. Each instance must have its
+ * index and fields to search configured.
+ */
+class IndexFulltextFilter extends SearchFilter {
+
+	protected $index;
+	protected $fields;
+	protected $fuzzy;
+
+	public function setIndex(SearchIndex $index) {
+		$this->index = $index;
+	}
+
+	public function setFields($fields) {
+		return $this->fields = $fields;
+	}
+
+	public function setFuzzy($fuzzy = true) {
+		$this->fuzzy = $fuzzy;
+	}
+
+	public function apply(DataQuery $data) {
+		$method = $this->fuzzy ? 'fuzzysearch' : 'search';
+
+		$query = new SearchQuery();
+		$query->$method($this->value, $this->fields);
+		$query->inClass($this->model);
+
+		$results = $this->index->search($query);
+		$ids = $results->Matches->column('ID');
+		$ids = array_map('intval', $ids);
+
+		return $data->where(sprintf(
+			'"%s"."ID" IN (%s)',
+			ClassInfo::baseDataClass($this->model),
+			implode(', ', $ids)
+		));
+	}
+
+	public function isEmpty() {
+		return !$this->getValue();
+	}
+
+}
