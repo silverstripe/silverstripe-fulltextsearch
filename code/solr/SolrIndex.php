silverstripe/silverstripe-fulltextsearch

commit a7629c8bb260fd6d23c04dee7520222ca0c070c5

BUG Remove non-displayable chars from Solr highlight results

code/solr/SolrIndex.php
@@ -312,7 +312,7 @@ abstract class SolrIndex extends SearchIndex {
 	 */
 	public function search(SearchQuery $query, $offset = -1, $limit = -1, $params = array()) {
 		$service = $this->getService();
-
+		
 		SearchVariant::with(count($query->classes) == 1 ? $query->classes[0]['class'] : null)->call('alterQuery', $query, $this);
 
 		$q = array();
@@ -440,6 +440,9 @@ abstract class SolrIndex extends SearchIndex {
 							$combinedHighlights = array_merge($combinedHighlights, $highlights);
 						}
 						$result->Excerpt = DBField::create_field('HTMLText', implode(' ... ', $combinedHighlights));
+						// Remove entity-encoded U+FFFD REPLACEMENT CHARACTER. 
+						// It signifies non-displayable characters, and shows up as such itself in browsers (questionmark icon)
+						$result->Excerpt = str_replace('&#65533;', '', $result->Excerpt);
 					}
 				}
 			}
@@ -447,7 +450,7 @@ abstract class SolrIndex extends SearchIndex {
 		} else {
 			$numFound = 0;
 		}
-		
+
 		$ret = array();
 		$ret['Matches'] = new PaginatedList($results);
 		$ret['Matches']->setLimitItems(false);

commit 3dfbfdb8ef849dadc794b66ffbf24d94377babbc

BUG: Ensure excerpts are returned as HTMLText instances.
a7629c8bb260fd6d23c04dee7520222ca0c070c5 inadvertently made it so
they were returned as plain strings due to the replace operation.

code/solr/SolrIndex.php
@@ -439,10 +439,17 @@ abstract class SolrIndex extends SearchIndex {
 						foreach($res->highlighting->$docId as $field => $highlights) {
 							$combinedHighlights = array_merge($combinedHighlights, $highlights);
 						}
-						$result->Excerpt = DBField::create_field('HTMLText', implode(' ... ', $combinedHighlights));
-						// Remove entity-encoded U+FFFD REPLACEMENT CHARACTER. 
-						// It signifies non-displayable characters, and shows up as such itself in browsers (questionmark icon)
-						$result->Excerpt = str_replace('&#65533;', '', $result->Excerpt);
+
+						// Remove entity-encoded U+FFFD replacement character. It signifies non-displayable characters,
+						// and shows up as an encoding error in browsers.
+						$result->Excerpt = DBField::create_field(
+							'HTMLText',
+							str_replace(
+								'&#65533;',
+								'',
+								implode(' ... ', $combinedHighlights)
+							)
+						);
 					}
 				}
 			}