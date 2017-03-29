f44ea37c91c5d436a3c73db8064b5d5aed229066
elliot sawyer <elliot@silverstripe.com>
2017-03-30 09:19:37 +1300

    Change the test to use appropriate assertions rather than asserting evaluated conditions are true/false
    Unit test fix: check for an empty array rather than an array with an object

--- tests/SearchVariantSubsitesTest.php
+++ tests/SearchVariantSubsitesTest.php
@@ -12,7 +12,7 @@ class SearchVariantSubsiteTest extends SapphireTest
 
         // Check versioned available
         if (!class_exists('Subsite')) {
-            return $this->markTestSkipped('The versioned decorator is not installed');
+            return $this->markTestSkipped('The subsites module is not installed');
         }
 
         if (self::$index === null) {
@@ -35,16 +35,18 @@ class SearchVariantSubsiteTest extends SapphireTest
         $query = new SearchQuery();
 
         //typical behaviour: nobody is explicitly filtering on subsite, so the search variant adds a filter to the query
-        $this->assertTrue(empty($query->require['_subsite']));
+        $this->assertEmpty($query->require['_subsite']);
         $variant = new SearchVariantSubsites();
         $variant->alterDefinition('SearchUpdaterTest_Container', $index);
         $variant->alterQuery($query, $index);
 
         //check that the "default" query has been put in place: it's not empty, and we're searching on Subsite ID:0 and
         // an object of SearchQuery::missing
-        $this->assertFalse(empty($query->require['_subsite']));
-        $this->assertTrue($query->require['_subsite'][0] == 0);
-        $this->assertTrue($query->require['_subsite'][1] instanceof SearchQuery::$missing);
+        $this->assertNotEmpty($query->require['_subsite']);
+        $this->assertEquals(0, $query->require['_subsite'][0]);
+
+        //check that SearchQuery::missing is set (by default, it is an object of stdClass)
+        $this->assertInstanceOf('stdClass', $query->require['_subsite'][1]);
     }
 
 
@@ -55,12 +57,13 @@ class SearchVariantSubsiteTest extends SapphireTest
         $query = new SearchQuery();
 
         //check that _subsite is not applied yet
-        $this->assertTrue(empty($query->require['_subsite']));
+        //this key should not be exist until the SearchVariant applies it later
+        $this->assertEmpty($query->require['_subsite']);
 
         //apply the subsite filter on the query (for example, if it's passed into a controller and set before searching)
         //we've chosen an arbirary value of 2 here, to check if it is changed later
         $query->filter('_subsite', 2);
-        $this->assertFalse(empty($query->require['_subsite']));
+        $this->assertNotEmpty($query->require['_subsite']);
 
         //apply the search variant's definition and query
         $variant = new SearchVariantSubsites();
@@ -71,16 +74,17 @@ class SearchVariantSubsiteTest extends SapphireTest
 
         //confirm that the query has been altered, but NOT with default values
         //first check that _subsite filter is not empty
-        $this->assertFalse(empty($query->require['_subsite']));
+        $this->assertNotEmpty($query->require['_subsite']);
         //subsite filter first value is not 0
-        $this->assertFalse($query->require['_subsite'][0] == 0);
-        //subsite filter second value is not SearchQuery::missing
-        $this->assertFalse($query->require['_subsite'][1] instanceof SearchQuery::$missing);
+        $this->assertNotEquals(0, $query->require['_subsite'][0]);
+
+        //subsite filter SearchQuery::missing should not be set so its expected location is empty
+        $this->assertEmpty($query->require['_subsite'][1]);
 
         //subsite filter has been modified with our arbitrary test value. The second value is not set
         //this proves that the query has not been altered by the variant
-        $this->assertTrue($query->require['_subsite'][0] == 2);
-        $this->assertTrue(empty($query->require['_subsite'][1]));
+        $this->assertEquals(2, $query->require['_subsite'][0]);
+
     }
 
 }
