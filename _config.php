1080d6444a32895fa103ecf974d0cc39cf6e8ddc
Robbie Averill <robbie@silverstripe.com>
2017-11-15 09:43:36 +1300

    FIX Update Travis configuration for SS4, add phpunit config and necessary composer updates

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
+
 use SilverStripe\FullTextSearch\Search\Updaters\SearchUpdater;
-global $databaseConfig;
-if (isset($databaseConfig['type'])) SearchUpdater::bind_manipulation_capture();
+
+if (isset($databaseConfig['type'])) {
+    SearchUpdater::bind_manipulation_capture();
+}
