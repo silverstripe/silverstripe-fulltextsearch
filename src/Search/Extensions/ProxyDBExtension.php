08052436e837c29094d95995a020b96eb8507574
Raissa North <raissa@silverstripe.com>
2018-03-15 09:45:14 +1300

    DOCS Add doc blocks for ProxyDBExtension

--- src/Search/Extensions/ProxyDBExtension.php
+++ src/Search/Extensions/ProxyDBExtension.php
@@ -6,8 +6,21 @@ use SilverStripe\Core\Extension;
 use TractorCow\ClassProxy\Generators\ProxyGenerator;
 use SilverStripe\FullTextSearch\Search\Updaters\SearchUpdater;
 
+/**
+ * Class ProxyDBExtension
+ * @package SilverStripe\FullTextSearch\Search\Extensions
+ *
+ * This database connector proxy will allow {@link SearchUpdater::handle_manipulation} to monitor database schema
+ * changes that may need to be propagated through to search indexes.
+ *
+ */
 class ProxyDBExtension extends Extension
 {
+    /**
+     * @param ProxyGenerator $proxy
+     *
+     * Ensure the search index is kept up to date by monitoring SilverStripe database manipulations
+     */
     public function updateProxy(ProxyGenerator &$proxy)
     {
         $proxy = $proxy->addMethod('manipulate', function ($args, $next) {
