4b9cf801b9eff5e5a4fd2692644daf31865ded05
Sabina Talipova <sabina.talipova@silverstripe.com>
2023-01-18 10:16:32 +1300

    API Remove deprcated code

--- src/Search/Queries/SearchQuery_Range.php
+++ src/Search/Queries/SearchQuery_Range.php
@@ -3,7 +3,6 @@
 namespace SilverStripe\FullTextSearch\Search\Queries;
 
 use SilverStripe\Core\Injector\Injectable;
-use SilverStripe\Dev\Deprecation;
 
 /**
  * Create one of these and pass as one of the values in filter or exclude to filter or exclude by a (possibly
@@ -37,25 +36,5 @@ class SearchQuery_Range
     public function isFiltered()
     {
         return $this->start !== null || $this->end !== null;
-    }
-
-    /**
-     * @deprecated 4.0.0 Use setStart() instead
-     * @codeCoverageIgnore
-     */
-    public function start($start)
-    {
-        Deprecation::notice('4.0.0', 'Use setStart() instead');
-        return $this->setStart($start);
-    }
-
-    /**
-     * @deprecated 4.0.0 Use setEnd() instead
-     * @codeCoverageIgnore
-     */
-    public function end($end)
-    {
-        Deprecation::notice('4.0.0', 'Use setEnd() instead');
-        return $this->setEnd($end);
     }
 }
